07c6ab83e337eb74e3c750506b8c6474723d3a9c
Sameer Jadhav <sammyjadhav6699@gmail.com>
2025-06-22 15:07:14 +0530

    Added API_BASE_URL configuration changes in the project

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const os = require('os');
 
 // Debug environment variables
 console.log('MongoDB URI:', process.env.MONGODB_URI);
@@ -49,9 +50,22 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: 'Something went wrong!' });
 });
 
+const getLocalIp = () => {
+    const nets = os.networkInterfaces();
+    for (const name of Object.keys(nets)) {
+        for (const net of nets[name]) {
+            if (net.family === 'IPv4' && !net.internal) {
+                return net.address;
+            }
+        }
+    }
+    return '0.0.0.0';
+};
+
 const PORT = process.env.PORT || 5000;
+const HOST = getLocalIp();
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
-    console.log(`Server is accessible at http://192.168.184.109:${PORT}`);
+    console.log(`Server is accessible at http://${HOST}:${PORT}`);
 }); 
